941c571218fc2c1ce4b8eab494fe28446c8688bf
DuKeM-CSGO <61344402+DuKeM-CSGO@users.noreply.github.com>
2021-10-02 17:11:10 +0800

    Add files via upload
    feat(router):添加路由（雏形）-- 获取最后一次登录的Steam账号ID，备份、还原游戏设置，启动CSGO

--- backend/router/router.go
+++ backend/router/router.go
@@ -17,10 +17,17 @@ func MiddlewareCORS(r *ghttp.Request) {
 func init() {
 	s := g.Server()
 	s.Group("/", func(group *ghttp.RouterGroup) {
-	    group.Middleware(MiddlewareCORS)
+		group.Middleware(MiddlewareCORS)
 		group.ALL("/hello", api.Hello)
 		group.Middleware(MiddlewareCORS)
 		group.ALL("/api/init", api.Init)
-
+		group.Middleware(MiddlewareCORS)
+		group.ALL("/api/refresh_steamacc", api.Refreshsteamacc)
+		group.Middleware(MiddlewareCORS)
+		group.ALL("/api/load_game_settings", api.LoadSettings)
+		group.Middleware(MiddlewareCORS)
+		group.ALL("/api/save_game_settings", api.SaveSettings)
+		group.Middleware(MiddlewareCORS)
+		group.ALL("/api/launch_csgo", api.CsgoLauncher)
 	})
 }
